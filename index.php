b54fb60405b13d4ea690dc921e8ae7e441ae7def
Hollie <tsubasababy1013@gmail.com>
2022-07-04 14:46:57 +0800

    把頁尾版權切版成footer.php

--- index.php
+++ index.php
@@ -85,14 +85,9 @@
                 </div>
                             </div>
              	<div style="clear:both;"></div>
-            	<div style="width:1024px; left:0px; position:relative; background:#FC3; margin-top:4px; height:123px; display:block;">
-                	<span class="t" style="line-height:123px;">
-									<?php
-										$bottom=new DB('bottom');
-										echo $bottom->find(1)['bottom'];
-									?>
-									</span>
-                </div>
+
+							<?php include "footer.php";?>
+							
     </div>
 
 </body></html>
