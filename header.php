864a97b18b679a8dc8d711b386406714102f2c3c
Hollie <tsubasababy1013@gmail.com>
2022-07-08 18:33:40 +0800

    完成標題區圖片的顯示

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <a title="" href="./index.php">
-	<div class="ti" style="background:url(&#39;img/<?=$Title->find(['sh'=>1])['img'];?>&#39;); 
-                         background-size:cover;">
+  <div class="ti" 
+  style="background:url(&#39;img/<?= $Title->find(['sh' => 1])['img']; ?>&#39;); background-size:cover;">
   </div>
-	<!--標題-->
+  <!--標題-->
 </a>
