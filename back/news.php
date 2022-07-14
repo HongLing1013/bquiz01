8cec161ae69a29d561f310bdb7bd1a8927ca0149
unknown <tsubasababy1013@gmail.com>
2022-07-14 09:31:25 +0800

    增加後臺列表分頁功能

--- back/news.php
+++ back/news.php
@@ -9,7 +9,16 @@
                     <td>刪除</td>
                 </tr>
                 <?php
-                    $rows=$DB->all();//撈出所有資料
+                    // 做分頁
+                    $all=$DB->math('count',"id");
+                    $div=4;//每4筆資料為一頁
+                    $pages=ceil($all/$div);//算頁數
+                    $now=$_GET['p']??1;//取得當前頁面
+                    $start=($now-1)*$div;//從哪裡開始計算
+                    $rows=$DB->all(" limit $start,$div");/* 撈出的資料是哪邊到哪邊
+                                                            這段語法等同select * from table limit 0,3 => 撈出 0.1.2
+                                                            或者select * from table limit 3,3 => 撈出 3.4.5
+                                                            或 select * from table limit 6,3 => 撈出 6.7.8 */
                     foreach($rows as $row){
                 ?>
                 <tr>
@@ -30,6 +39,25 @@
                 ?>
             </tbody>
         </table>
+        <div class="cent">
+            <?php
+                    if(($now-1) > 0){//如果有上一頁的話 就顯示<的按鈕
+                        $p=$now-1;
+                        echo "<a href='?do={$Str->table}&p=$p'> < </a>";
+                    }
+                    for($i=1;$i<=$pages;$i++){// 印頁碼
+                        $fontsize=($now==$i)?'1.5rem':'';//當前頁數字體放大
+                        echo "<a href='?do={$Str->table}&p=$i' style='font-size:$fontsize'> ";//?代表當前頁面
+                        echo $i;
+                        echo " </a>";
+                    }
+                    if(($now+1) <= $pages){//如果有下一頁的話 就顯示>的按鈕
+                        $p=$now+1;
+                        echo "<a href='?do={$Str->table}&p=$p'> > </a>";
+                    }
+                ?>
+
+        </div>
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
